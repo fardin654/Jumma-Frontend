03093162b761f906e269de46312c63d470d0e299
fardin654 <fardinmd654@gmail.com>
2025-08-16 00:23:31 +0530

    First Commit

---
+++ src/context/WalletContext.jsx
@@ -0,0 +1,50 @@
+// src/context/WalletContext.js
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
+
+export const WalletContext = createContext();
+
+export const WalletProvider = ({ children }) => {
+  const [balance, setBalance] = useState(0);
+  const [loading, setLoading] = useState(true);
+
+  const fetchWalletBalance = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/wallets");
+      setBalance(res.data.Balance);
+      console.log("Balance Fetched: ", balance);
+      setLoading(false);
+    } catch (err) {
+      console.error("Error fetching wallet balance:", err);
+      setLoading(false);
+    }
+  };
+
+  const updateWalletBalance = async (newBalance) => {
+    try {
+      const res = await axios.patch("http://localhost:5000/api/wallets", { Balance: newBalance });
+      setBalance(res.data.Balance);
+      return res.data;
+    } catch (err) {
+      console.error("Error updating wallet balance:", err);
+      throw err;
+    }
+  };
+
+  useEffect(() => {
+    fetchWalletBalance();
+  }, []);
+
+  return (
+    <WalletContext.Provider
+      value={{
+        balance,
+        loading,
+        fetchWalletBalance,
+        updateWalletBalance,
+      }}
+    >
+      {children}
+    </WalletContext.Provider>
+  );
+};
