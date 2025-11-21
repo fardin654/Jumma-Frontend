6bae43fb329a0c2f489f5653524d0cf8b4a0c1be
Md Fardin <154866906+fardin654@users.noreply.github.com>
2025-11-22 02:09:59 +0530

    Switch API URL to production endpoint

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-// export const URL = "https://jumma-backend-vercel.vercel.app/api";
-export const URL = "http://localhost:5000/api";
+export const URL = "https://jumma-backend-vercel.vercel.app/api";
+// export const URL = "http://localhost:5000/api";
 // export const URL = "https://jumma-backend.onrender.com/api";
